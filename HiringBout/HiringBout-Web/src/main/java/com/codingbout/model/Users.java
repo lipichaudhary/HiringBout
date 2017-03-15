22f4ccc06d77680cd4ba18e43e8349d94c7eaa93
Rishav Mishra <mrishav72@gmail.com>
2017-03-16 00:06:07 +0530

    Added User.java model class

---
+++ HiringBout/HiringBout-Web/src/main/java/com/codingbout/model/Users.java
@@ -0,0 +1,52 @@
+package com.codingbout.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+@SuppressWarnings("serial")
+public class Users implements Serializable{
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id", length=11)
+	private Long id;
+	
+	@Column
+	String username;
+	
+	@Column
+	String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
