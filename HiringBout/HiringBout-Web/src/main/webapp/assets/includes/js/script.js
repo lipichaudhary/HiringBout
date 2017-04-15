f463e699b301b1e77004e85c030d4bee18a3c03f
Rishav Mishra <mrishav72@gmail.com>
2017-04-15 23:59:17 +0530

    Edited script.js and style.css for supporting validation in login and registration flows

--- HiringBout/HiringBout-Web/src/main/webapp/assets/includes/js/script.js
+++ HiringBout/HiringBout-Web/src/main/webapp/assets/includes/js/script.js
@@ -1,10 +1,19 @@
-/*
-
-My Custom JS
-============
-
-Author:  Brad Hussey
-Updated: August 2013
-Notes:	 Hand coded for Udemy.com
-
-*/
+	function validateLoginForm(){
+		var accusername = document.forms["login-form"]["username"].value;
+		var accpassword = document.forms["login-form"]["password"].value;
+		if(accusername=="" || accpassword==""){
+			document.getElementById('loginErrors').style.display = 'block';
+		     return false;
+		}
+	}
+	
+	function ValidateRegistrationForm(){
+		var emailaddress = document.forms["Registration-form"]["emailaddress"].value;
+		var accpassword = document.forms["Registration-form"]["accpassword"].value;
+		var accusername = document.forms["Registration-form"]["accusername"].value;
+		if(accusername=="" || accpassword=="" || emailaddress==""){
+			documnent.getElementById('registrationError').style.display = "block";
+			return false;
+		}
+		
+	}
